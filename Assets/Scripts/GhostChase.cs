5556356667c6b8ea65a7d47997af862590545a5d
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-08-12 16:31:35 +0700

    scatter and chase

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -2,5 +2,34 @@ using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
-    
+    private void OnDisable()
+    {
+        this.ghost.scatter.Enable();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+        if (node != null && this.enabled && !this.ghost.frightened.enabled && node.availableDirections.Count > 0)
+        {
+            this.ghost.movement.rb.position = node.transform.position;
+            Vector2 bestDirection = node.availableDirections[0];
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                if (availableDirection == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+                    continue;
+
+                Vector3 targetPosition = this.transform.position + (Vector3)availableDirection;
+                float distance = (this.ghost.target.position - targetPosition).sqrMagnitude;
+                if (distance < minDistance)
+                {
+                    bestDirection = availableDirection;
+                    minDistance = distance;
+                }
+            }
+            this.ghost.movement.SetDirection(bestDirection);
+        }
+
+    }
 }
